799c849a6249a03be420007d561138efad870233
Vinicius Moralis <vinicius_moralis@hotmail.com>
2020-03-23 17:21:28 -0300

    modificando tela principal

---
+++ app/src/main/java/com/example/atmconsultoria/ui/principal/PrincipalFragment.java
@@ -0,0 +1,22 @@
+package com.example.atmconsultoria.ui.principal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.atmconsultoria.R;
+
+public class PrincipalFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_principal, container, false);
+        return root;
+    }
+}
